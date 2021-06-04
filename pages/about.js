06e84e7d9aa0534d1e159bbbd6ec34d888b278ba
Veljko28 <charlibear28@gmail.com>
2021-06-04 10:48:13 +0200

    adding the static pages

--- pages/about.js
+++ pages/about.js
@@ -10,7 +10,7 @@ export default function about() {
       <h1 className={styles.header}>About</h1>
       <div className={styles.main}>
         <p className={styles.paragraph}>
-        <h3 style={{textAlign: 'center'}}>About the site</h3>  
+        <span style={{textAlign: 'center'}}>About the site</span>  
         This is a test website that fetches articles from an api 
         <a href="https://jsonplaceholder.typicode.com/" style={{color: '#00f'}}> JsonPlaceholder</a> and displays the articles using
         react and next.js which generates the routes for all the articles. It uses javascript and css for the majority of content
@@ -18,7 +18,7 @@ export default function about() {
 
 
         <p className={styles.paragraph}>
-        <h3 style={{textAlign: 'center'}}>About Next.js</h3>  
+        <span style={{textAlign: 'center'}}>About Next.js</span>  
         Next.js is an open-source React front-end development web framework created by Vercel that enables functionality such as server-side rendering and generating static websites for React based applications. It's a production-ready framework that allows developers to create static and dynamic websites.</p>
       </div>
     </>
--- pages/articles.js
+++ pages/articles.js
@@ -1,8 +1,34 @@
+import Head from 'next/head';
+import Article from '../components/Article';
+import styles from '../styles/Articles.module.css';
 
-export default function article() {
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10');
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles
+    }
+  }
+}
+
+
+export default function article({articles}) {
   return (
     <div>
-      Articles
+      <Head>
+        <title>Articles</title>
+      </Head>
+      <h1 className={styles.header}>Articles</h1>
+      <main className={styles.main}>
+        <div className={styles.grid}>
+          {articles.map(x => {
+            return (<Article article={x} />)
+          })}
+        </div>
+      </main>
     </div>
   )
 }
